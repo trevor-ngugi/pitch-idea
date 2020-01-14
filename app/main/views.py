dca30a5fd77221901d286d27075d323248891574
trevor ngugi <ngugitrevor@gmail.com>
2020-01-14 10:05:58 +0300

    add psots table

--- app/main/views.py
+++ app/main/views.py
@@ -3,3 +3,32 @@ from . import main
 from flask_login import login_required
 # from .forms import 
 # from ..models import Review
+
+@main.route("/", methods = ["GET", "POST"])
+@login_required
+def index():
+    """
+    View root page function that returns the index page and its data
+    """
+    all_posts = Post.query.all()
+    post_form = PostForm()
+    title = "60 Seconds | Leave a mark"
+
+    if post_form.validate_on_submit():
+        post_title = post_form.title.data
+        post_form.title.data = ""
+        post_content = post_form.post.data
+        post_form.post.data = ""
+        post_category = post_form.category.data
+        new_post = Post(post_title = post_title,
+                        post_content = post_content,
+                        category = post_category,
+                        user_id = current_user.id)
+
+        new_post.save_post()
+        return redirect(url_for("main.index"))
+    
+    return render_template("index.html",
+                            title = title,
+                            post_form = post_form,
+                            all_posts = all_posts)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
+from datetime import datetime
+
 
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
@@ -8,6 +10,7 @@ class User(UserMixin,db.Model):
     full_name = db.Column(db.String(255))
     username = db.Column(db.String(255))
     pass_secure = db.Column(db.String(255))
+    posts = db.relationship("Post",backref = "posts",lazy = "dynamic")
 
     @property
     def password(self):
@@ -23,3 +26,23 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    post_id = db.Column(db.Integer,primary_key = True)
+    post_title = db.Column(db.String)
+    post_content = db.Column(db.String)
+    posted_at = db.Column(db.DateTime,default=datetime.utcnow)
+    category = db.Column(db.String)
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    # comments = db.relationship("Comment",backref = "post_comments",lazy = "dynamic")
+
+    def save_post(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_user_posts(cls,id):
+        posts = Post.query.filter_by(user_id=id).all()
+        return posts
